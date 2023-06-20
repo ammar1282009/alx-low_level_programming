01a6fb344522df1697205235b0268e9a17e056e5
root <you@example.com>
2023-06-20 11:25:21 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the times table to print
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
+	int i, j, k;
 
-	int i, j;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				printf("%d", i * j);
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else if (i * j < 10)
-			{
-				printf("   %d", i * j);
-			}
-			else if (i * j < 100)
-			{
-				printf("  %d", i * j);
-			}
-			else
-			{
-				printf(" %d", i * j);
-			}
-
-			if (j != n)
-			{
-				printf(",");
-			}
-			else
-			{
-				printf("\n");
-			}
+			_putchar('\n');
 		}
 	}
 }
